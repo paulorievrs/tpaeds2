572add5ea022b34596dca2c23fb6d51ca1cf8444
paulorievrs <prievrs@gmail.com>
2020-03-20 16:44:20 -0300

    TP02

---
+++ TP02/C/BubbleSort/bubblesort.cpp
@@ -0,0 +1,304 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define TAM 1000
+
+#define SZ 1000
+
+char* lerArquivo(char *nome);
+
+typedef struct Personagem {
+   char *nome;
+   int altura;
+   double peso;
+   char *corDoCabelo;
+   char *corDaPele;
+   char *corDosOlhos;
+   char *anoNascimento;
+   char *genero;
+   char *homeworld;
+
+} Personagem;
+
+void setPersonagem(Personagem personagem, char *arquivo);
+char *acharNome(char *s);
+int altura(char *s);
+double massa(char *s);
+char *corDoCabelo(char *s);
+char *corDaPele(char *s);
+char *nascimento(char *s);
+char *corDosOlhos(char *s);
+char *genero(char *s);
+char *homeworld(char *s);
+char* removerSpacos(char str[]);
+
+
+Personagem personagemA[TAM];
+int n = 0;
+
+
+//BUBBLE
+void bubbleSort() {
+   for (int i = (n - 1); i > 0; i--) {
+      for (int j = 0; j < i; j++) {
+         if (strcmp(personagemA[j].anoNascimento, personagemA[j + 1].anoNascimento) > 0) {
+            Personagem auxiliar = personagemA[j + 1];
+            personagemA[j + 1] = personagemA[j];
+            personagemA[j] = auxiliar;
+         }
+      }
+   }
+}
+
+
+void inserirFim(Personagem personagem) {
+      if (n >= TAM) {
+            printf("Erro ao inserirFim com");
+            exit(1);
+      }
+      personagemA[n] = personagem;
+      n++;
+}
+
+char *lerArquivo(char *nome) {
+      FILE *arq;
+      char *arquivo = (char*) malloc(SZ * sizeof(char));
+      arq = fopen(nome, "r");
+      fgets(arquivo, 1000, arq);
+      fclose(arq);
+      return arquivo; 
+}
+
+bool isFim(char *s){
+    return (strlen(s) >= 3 && s[0] == 'F' && s[1] == 'I' && s[2] == 'M');
+}
+
+char *acharNome(char *s) {
+      char *nome = (char*) malloc(SZ * sizeof(char));
+      int j = 0;
+      int p = 0;
+      char aspas = '\'';
+
+      for (int i = 0; i < strlen(s); i++) {
+            if (s[i] == 'n' && s[i+1] == 'a' && s[i+2] == 'm' && s[i+3] == 'e') {
+                  j = i+8;
+
+                  for(int k = j; k < strlen(s); k++) {
+                        if(s[k] == aspas) {
+                              nome[p] = '\0';
+                              k = strlen(s);
+                        } else {
+                              nome[p] = s[k];
+                              p++;
+                        }
+                  }
+            }
+      }
+      nome = removerSpacos(nome);
+      return nome;
+
+}
+
+int altura(char *s) {
+      char *altura = (char*) malloc(SZ * sizeof(char));
+      int j = 0;
+      int p = 0;
+      char aspas = '\'';
+      for (int i = 0; i < strlen(s); i++) {
+            if (s[i] == 'h' && s[i+1] == 'e' && s[i+2] == 'i' && s[i+3] == 'g' && s[i+4] == 'h' && s[i+5] == 't') {
+                  j = i+10;
+                  while (s[j] != aspas) {
+                        altura[p] = s[j];
+                        p++;
+                        j++;
+                  }
+            }     
+      }
+      return atoi(altura);
+}
+
+double massa(char *s) {
+      char *massa = (char*) malloc(SZ * sizeof(char));
+      int j = 0;
+      int p = 0;
+      char aspas = '\'';
+      for (int i = 0; i < strlen(s); i++) {
+            if (s[i] == 'm' && s[(i+1)] == 'a' && s[(i+2)] == 's' && s[(i+3)] == 's') {
+                  j = i+8;
+                  while (s[(j)] != aspas) {
+                        if(s[j] != ',') {
+                              massa[p] = s[j];
+                              p++;
+                        }
+                        j++;
+                  }
+            }
+      }
+      return atof(massa);
+}
+
+char *corDoCabelo(char *s) {
+      char *cor = (char*) malloc (SZ * sizeof(char));
+      int j = 0;
+      int p = 0;
+      char aspas = '\'';
+      for (int i = 0; i < strlen(s); i++) {
+            if (s[(i)] == 'h' && s[(i+1)] == 'a' && s[(i+2)] == 'i' && s[(i+3)] == 'r') {
+                  j = i+14;
+                  while (s[j] != aspas) {
+                        cor[p] = s[j];
+                        p++;
+                        j++;
+                  }
+            }
+      }
+      return cor;
+}
+
+
+char *corDaPele(char *s) {
+      char *cor = (char*) malloc (SZ * sizeof(char));
+      int j = 0;
+      int p = 0;
+      char aspas = '\'';
+      for (int i = 0; i < strlen(s); i++) {
+            if (s[i] == 's' && s[i+1] == 'k' && s[(i+2)] == 'i' && s[(i+3)] == 'n') {
+                  j = i+14;
+                  while (s[(j)] != aspas) {
+                        cor[p] += s[(j)];
+                        p++;
+                        j++;
+                  }
+            }
+      }
+      return cor;
+}
+
+char *corDosOlhos(char *s) {
+      char *cor = (char*) malloc(SZ * sizeof(char));
+      int j = 0;
+      int p = 0;
+      char aspas = '\'';
+      for (int i = 0; i < strlen(s); i++) {
+            if (s[(i)] == 'e' && s[(i+1)] == 'y' && s[(i+2)] == 'e') {
+                  j = i+13;
+                  while (s[(j)] != aspas) {
+                        cor[p] += s[(j)];
+                        p++;
+                        j++;
+                  }
+            }
+      }
+      return cor;
+}
+
+char *nascimento(char *s) {
+      char *nascimento = (char*) malloc(SZ * sizeof(char));
+      int j = 0;
+      int p = 0;
+      char aspas = '\'';
+      for (int i = 0; i < strlen(s); i++) {
+            if (s[(i)] == 'b' && s[(i+1)] == 'i' && s[(i+2)] == 'r' && s[(i+3)] == 't' && s[(i+4)] == 'h') {
+                  j = i+14;
+                  while (s[(j)] != aspas) {
+                        nascimento[p] += s[(j)];
+                        p++;
+                        j++;
+                  }
+            }
+      }
+      return nascimento;
+}
+
+char *genero (char *s) {
+      char *genero = (char*) malloc(SZ * sizeof(char));
+      int j = 0;
+      int p = 0;
+      char aspas = '\'';
+      for (int i = 0; i < strlen(s); i++) {
+            if (s[(i)] == 'g' && s[(i+1)] == 'e' && s[(i+2)] == 'n' && s[(i+3)] == 'd' && s[(i+4)] == 'e' && s[(i+5)] == 'r') {
+                  j = i+10;
+                  while (s[(j)] != aspas) {
+                        genero[p] += s[(j)];
+                        p++;
+                        j++;
+                  }
+            }
+      }
+      return genero;
+}
+
+char *homeworld(char *s) {
+      char *homeworld = (char*) malloc(SZ * sizeof(char));
+      int j = 0;
+      int p = 0;
+      char aspas = '\'';
+      for (int i = 0; i < strlen(s); i++) {
+            if (s[(i)] == 'h' && s[(i+1)] == 'o' && s[(i+2)] == 'm' && s[(i+3)] == 'e' && s[(i+4)] == 'w' && s[(i+5)] == 'o' && s[(i+6)] == 'r' && s[(i+7)] == 'l' && s[(i+8)] == 'd') {
+                  j = i+13;
+                  while (s[(j)] != aspas) {
+                        homeworld[p] += s[(j)];
+                        p++;
+                        j++;
+                  }
+            }
+      }
+      return homeworld;
+}
+
+char* removerSpacos(char str[]) {
+      int j = 1;
+      for (int i = 1; str[i]; i++) {
+            if(str[i] != ' ' || (str[i - 1] != ' ')) {
+                  str[j] = str[i];
+                  j++;
+            }
+      }
+      str[j] = '\0';
+      return str;
+}
+
+
+void printPersonagem(Personagem personagem) {
+      printf("## %s ## %d ## %g ## %s ## %s ## %s ## %s ## %s ## %s ## \n", personagem.nome, personagem.altura, personagem.peso, personagem.corDoCabelo, personagem.corDaPele, personagem.corDosOlhos, personagem.anoNascimento, personagem.genero, personagem.homeworld);
+}
+
+
+
+int main() {
+
+      char entrada[SZ][SZ]; //Array de string (matriz de char)
+      int quantidadeEntrada = 0;
+      char *arquivo; //armazenar nome do arquivo
+      
+      //ler até aparecer fim na entrada padrão
+      do {
+            scanf(" %[^\n]", entrada[quantidadeEntrada]);
+      } while(isFim(entrada[quantidadeEntrada++]) == false);
+      quantidadeEntrada--;
+
+      Personagem personagemEntradaPadrao[TAM]; //Array para armazenar os personagens da entrada padrão      
+      //Armazenar personagens do pub in na lista
+      for(int i = 0; i < quantidadeEntrada; i++) {
+
+            arquivo = lerArquivo(entrada[i]); 
+            personagemEntradaPadrao[i].nome = acharNome(arquivo);
+            personagemEntradaPadrao[i].altura = altura(arquivo);
+            personagemEntradaPadrao[i].peso = massa(arquivo);
+            personagemEntradaPadrao[i].corDoCabelo = corDoCabelo(arquivo);
+            personagemEntradaPadrao[i].corDaPele = corDaPele(arquivo);
+            personagemEntradaPadrao[i].anoNascimento = nascimento(arquivo);
+            personagemEntradaPadrao[i].corDosOlhos = corDosOlhos(arquivo);
+            personagemEntradaPadrao[i].genero = genero(arquivo);
+            personagemEntradaPadrao[i].homeworld = homeworld(arquivo);
+            inserirFim(personagemEntradaPadrao[i]);
+      }
+
+      bubbleSort();
+
+      for(int i = 0; i < quantidadeEntrada; i++) {
+            printPersonagem(personagemA[i]);
+      }
+}
---
+++ TP02/Java/Counting Sort/Counting.java
@@ -0,0 +1,591 @@
+import java.text.*;
+import java.io.*;
+import java.util.*;
+/*
+* Classe Personagem TP02 Q01 - Java
+* @author Paulo Rievrs Oliveira
+* @version 1 02/2020
+*/
+
+
+class Personagem {
+	//Atributos privados	
+	private String nome = "";
+	private int altura = 0;
+	private double peso = 0;
+	private String corDoCabelo = "";
+	private String corDaPele = "";
+	private String corDosOlhos = "";
+	private String anoNascimento = "";
+	private String genero = "";
+	private String homeworld= "";
+
+	//Construtor Vazio
+	public Personagem() {
+		this.nome = "";
+		this.altura = 0;
+		this.peso = 0;
+		this.corDoCabelo = "";
+		this.corDaPele = "";
+		this.corDosOlhos = "";
+		this.anoNascimento = "";
+		this.genero = "";
+		this.homeworld= "";
+	}	
+
+	//Construtor que recebe parametros
+	public Personagem(String nome, int altura, double peso, String corDoCabelo, String corDaPele, String corDosOlhos, String anoNascimento, String genero, String homeworld) {
+		
+		setNome(nome);
+		setAltura(altura);
+		setPeso(peso);
+		setCorDoCabelo(corDoCabelo);
+		setCorDaPele(corDaPele);
+		setCorDosOlhos(corDosOlhos);
+		setAnoNascimento(anoNascimento);
+		setGenero(genero);
+		setHomeworld(homeworld);
+
+	}
+
+	//Inicio get's e set's dos atributos
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	public int getAltura(){
+		return this.altura;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	public double getPeso(){
+		return this.peso;
+	}
+	public void setCorDoCabelo(String corDoCabelo) {
+		this.corDoCabelo = corDoCabelo;
+	}
+	public String getCorDoCabelo(){
+		return this.corDoCabelo;
+	}
+	public void setCorDaPele(String corDaPele) {
+		this.corDaPele = corDaPele;
+	}
+	public String getCorDaPele(){
+		return this.corDaPele;
+	}
+	public void setCorDosOlhos(String corDosOlhos) {
+		this.corDosOlhos = corDosOlhos;
+	}
+	public String getCorDosOlhos(){
+		return this.corDosOlhos;
+	}
+	public void setAnoNascimento(String anoNascimento) {
+		this.anoNascimento = anoNascimento;
+	}
+	public String getAnoNascimento(){
+		return this.anoNascimento;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public String getGenero(){
+		return this.genero;
+	}
+	public void setHomeworld(String homeworld) {
+		this.homeworld = homeworld;
+	}
+	public String getHomeworld(){
+		return this.homeworld;
+	}
+	//fim set's e get's
+
+	//metodo de printar
+	public void print() throws Exception{
+		DecimalFormat df = new DecimalFormat("#.#", new DecimalFormatSymbols(Locale.US));
+		    
+		String print = (" ## "+this.getNome()+" ## "+df.format(this.getAltura())+" ## "+df.format(this.getPeso())+" ## "+this.getCorDoCabelo()+" ## "+this.getCorDaPele()+
+		" ## "+this.getCorDosOlhos()+" ## "+this.getAnoNascimento()+" ## "+this.getGenero()+ " ## "+this.getHomeworld()+" ## ");
+		String convertida = new String(print.getBytes(), "ISO-8859-1");
+		MyIO.println(convertida);
+	}
+
+	public Personagem clone() {
+		Personagem clone = new Personagem(getNome(), 
+			getAltura(), 
+			getPeso(), 
+			getCorDoCabelo(), 
+			getCorDaPele(), 
+			getCorDosOlhos(), 
+			getAnoNascimento(), 
+			getGenero(), 
+			getHomeworld());
+
+		return clone;
+	}
+	//ler dados do arquivo
+	public String lerArquivo(String arquivo) {
+		String linha = new String();
+		try {
+			FileReader arq = new FileReader(arquivo);
+			BufferedReader lerArq = new BufferedReader(arq);
+			linha = lerArq.readLine(); 
+			arq.close();
+
+		    } catch (IOException e) {
+			  System.err.printf("Erro na abertura do arquivo: %s.\n",
+			    e.getMessage());
+		    }
+		return linha;
+	}
+	
+	//manipular dados
+	//metodo para achar nome
+	public void acharNome(String s) {
+		String nome = new String();
+		int j = 0;
+		char aspas = '\'';
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'n' && s.charAt(i+1) == 'a' && s.charAt(i+2) == 'm' && s.charAt(i+3) == 'e') {
+				j = i+8;
+				while (s.charAt(j) != aspas) {
+					nome += s.charAt(j);
+					j++;
+				}
+			}
+		}
+		this.setNome(nome);
+	}
+
+	//metodo para achar a altura
+	public void altura(String s) {
+		String altura = "";
+		int j = 0;
+		char aspas = '\'';
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'h' && s.charAt(i+1) == 'e' && s.charAt(i+2) == 'i' && s.charAt(i+3) == 'g' && s.charAt(i+4) == 'h' && s.charAt(i+5) == 't') {
+				j = i+10;
+				while (s.charAt(j) != aspas) {
+					altura += s.charAt(j);
+					j++;
+				}
+			}
+			
+		}
+
+		int alt = 0;
+		try {
+			alt = Integer.parseInt(altura);
+		} catch (NumberFormatException e) {
+			alt = 0;
+		}
+		this.setAltura(alt);
+	}
+	//metodo para achar o peso
+	public void peso(String s) {
+		NumberFormat format = NumberFormat.getNumberInstance(Locale.ENGLISH);
+		format.setMinimumFractionDigits(0);
+		String p = "";
+		int j = 0;
+		char aspas = '\'';
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'm' && s.charAt(i+1) == 'a' && s.charAt(i+2) == 's' && s.charAt(i+3) == 's') {
+				j = i+8;
+				while (s.charAt(j) != aspas) {
+					p += s.charAt(j);
+					j++;
+				}
+			}
+		}
+		double peso = 0;
+		p = removeDig(p, ",");	
+		try {
+			peso = Double.parseDouble(p);
+		} catch (NumberFormatException e) {
+			//nada
+		}
+		this.setPeso(peso);
+
+	}
+	//metodo para retirar virgula
+	public String removeDig(String s, String dig) {
+		int cont = 0;
+		
+		for(int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == dig.charAt(0)) {
+
+				cont++;
+			}
+		}
+		String nova = new String();
+		String[] split = new String[cont];
+		split = s.split(dig);
+		for (int i = 0; i < split.length; i++) {
+			nova+=split[i];
+		}
+
+		return nova;
+	}
+	//metodo para achar a cor do cabelo
+	public void corDoCabelo(String s) {
+		String cor = "";
+		int j = 0;
+		char aspas = '\'';
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'h' && s.charAt(i+1) == 'a' && s.charAt(i+2) == 'i' && s.charAt(i+3) == 'r') {
+				j = i+14;
+				while (s.charAt(j) != aspas) {
+					cor += s.charAt(j);
+					j++;
+				}
+			}
+		}
+		this.setCorDoCabelo(cor);
+	}
+
+	//metodo para achar a cor da pele
+	public void corDaPele(String s) {
+		String cor = "";
+		int j = 0;
+		char aspas = '\'';
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 's' && s.charAt(i+1) == 'k' && s.charAt(i+2) == 'i' && s.charAt(i+3) == 'n') {
+				j = i+14;
+				while (s.charAt(j) != aspas) {
+					cor += s.charAt(j);
+					j++;
+				}
+			}
+		}
+		this.setCorDaPele(cor);
+	}
+
+	//metodo para achar a cor dos olhos
+	public void corDosOlhos(String s) {
+		String cor = "";
+		int j = 0;
+		char aspas = '\'';
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'e' && s.charAt(i+1) == 'y' && s.charAt(i+2) == 'e') {
+				j = i+13;
+				while (s.charAt(j) != aspas) {
+					cor += s.charAt(j);
+					j++;
+				}
+			}
+		}
+		this.setCorDosOlhos(cor);
+	}
+
+	//metodo para achar o ano de nascimento
+	public void nascimento(String s) {
+		String nascimento = "";
+		int j = 0;
+		char aspas = '\'';
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'b' && s.charAt(i+1) == 'i' && s.charAt(i+2) == 'r' && s.charAt(i+3) == 't' && s.charAt(i+4) == 'h') {
+				j = i+14;
+				while (s.charAt(j) != aspas) {
+					nascimento += s.charAt(j);
+					j++;
+				}
+			}
+		}
+		this.setAnoNascimento(nascimento);
+	}
+
+	//metodo para achar o genero
+	public void genero(String s) {
+		String genero = "";
+		int j = 0;
+		char aspas = '\'';
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'g' && s.charAt(i+1) == 'e' && s.charAt(i+2) == 'n' && s.charAt(i+3) == 'd' && s.charAt(i+4) == 'e' && s.charAt(i+5) == 'r') {
+				j = i+10;
+				while (s.charAt(j) != aspas) {
+					genero += s.charAt(j);
+					j++;
+				}
+			}
+		}
+		this.setGenero(genero);
+	}
+	
+	//metodo  para achar o homeworld
+	public void homeworld(String s) {
+		String homeworld = "";
+		int j = 0;
+		char aspas = '\'';
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'h' && s.charAt(i+1) == 'o' && s.charAt(i+2) == 'm' && s.charAt(i+3) == 'e' && s.charAt(i+4) == 'w' && s.charAt(i+5) == 'o' && s.charAt(i+6) == 'r' && s.charAt(i+7) == 'l' && s.charAt(i+8) == 'd') {
+				j = i+13;
+				while (s.charAt(j) != aspas) {
+					homeworld += s.charAt(j);
+					j++;
+				}
+			}
+		}
+		this.setHomeworld(homeworld);
+	}
+	
+}
+
+class MetodosLista {
+      protected Personagem[] personagemA;
+      protected int n;
+
+      public MetodosLista() {
+		this(1000);
+	}
+
+	public MetodosLista(int tam) {
+		personagemA = new Personagem[tam];
+		n = 0;
+	}
+
+	public void setN(int n) {
+		this.n = n;
+	}
+	public int getN() {
+		return this.n;
+	}
+	public void setPersonagensA(int posicao, Personagem[] personagemA) {
+		this.personagemA = personagemA.clone();
+	}
+	public Personagem[] getPersonagemA() {
+		return this.personagemA;
+	}
+
+	
+
+      public void inserirInicio(Personagem personagem) throws Exception {
+            if (n >= personagemA.length) {
+                  throw new Exception("Erro ao inserirInicio coom:" + personagem);
+            }
+
+            for(int i = n; i > 0; i--) {
+                  personagemA[i] = personagemA[i-1].clone();
+            }
+            personagemA[0] = personagem.clone();
+            n++;
+      }
+
+      public void inserir(Personagem personagem, int posicao) throws Exception{
+            if (n >= personagemA.length ||posicao < 0 || posicao > n) {
+                  throw new Exception("Erro ao inserir com:" + personagem);
+            }
+            for(int i = n; i > posicao; i--) {
+                  personagemA[i] = personagemA[i - 1].clone();
+            }
+            personagemA[posicao] = personagem.clone();
+            n++;
+      }
+
+      public void inserirFim(Personagem personagem) throws Exception {
+            if (n >= personagemA.length) {
+                  throw new Exception("Erro ao inserirFim com: " + personagem);
+            }
+            personagemA[n] = personagem.clone();
+            n++;
+      }
+      
+      public Personagem removerInicio()throws Exception {
+            if (n == 0) {
+                  throw new Exception("Erro ao removerInicio ");
+            }
+            Personagem personagemRetorno = personagemA[0].clone();
+            n--;
+            for(int i = 0; i < n; i++) {
+                  personagemA[i] = personagemA[i+1].clone();
+            }
+
+            return personagemRetorno;
+
+      }
+
+      public Personagem remover(int posicao) throws Exception{
+            if (n == 0 || posicao < 0 || posicao >= n) {
+
+                  throw new Exception("Erro ao remover, n = " + n + " posicao = " + posicao);
+		}
+		n--;
+		
+            Personagem personagemRetorno = personagemA[posicao].clone();
+            for (int i = posicao; i < n; i++) {
+                  personagemA[i] = personagemA[i + 1].clone();
+            }
+            return personagemRetorno;
+      }
+
+      public Personagem removerFim() throws Exception {
+            if (n == 0) {
+                  throw new Exception("Erro removerFim");
+            }
+            return personagemA[--n];
+	}
+	
+	
+}
+
+class Countingsort extends MetodosLista {
+	private int quantidadeDeMovimentacoes = 0;
+
+
+    public Countingsort(){
+        super();
+     }
+  
+  
+      /**
+       * Construtor.
+       * @param int tamanho do array de numeros inteiros.
+       */
+     public Countingsort(int tam){
+        super(tam);
+     }
+
+
+	public int getQuantidadeDeMovimentacoes() {
+		return this.quantidadeDeMovimentacoes;
+	}
+
+      /**
+       * Retorna o maior elemento do array.
+      * @return maior elemento
+       */
+      public int getMaior() {
+         Personagem maior = personagemA[0].clone();
+  
+          for (int i = 0; i < n; i++) {
+           if(maior.getPeso() < personagemA[i].getPeso()) {
+              maior = personagemA[i].clone();
+           }
+          }
+         return maior;	
+      }
+  
+  
+      /**
+       * Algoritmo de ordenacao Countingsort.
+       */
+      public void countingsort() {
+        //Array para contar o numero de ocorrencias de cada elemento
+        Personagem[] count = new Personagem[getMaior() + 1];
+        Personagem[] ordenado = new Personagem[n];
+  
+        //Inicializar cada posicao do array de contagem 
+        for (int i = 0; i < count.length; i++) {
+            count[i].setPeso(0);
+        }
+  
+        //Agora, o count[i] contem o numero de elemento iguais a i
+        for (int i = 0; i < n; i++) {
+            count[personagemA[i].getPeso()]++;
+        }
+        
+        //Agora, o count[i] contem o numero de elemento menores ou iguais a i
+        for(int i = 1; i < count.length; i++) {
+            Double peso = count[i].getPeso();
+            count[i].setPeso(peso += count[i - 1].getPeso());
+        }
+  
+        //Ordenando
+        for(int i = n - 1; i >= 0; i--) {
+            
+
+            ordenado[count[personagemA[i].getPeso()] - 1] = personagemA[i].clone();
+            count[personagemA[i].getṔeso()]--;
+        }
+  
+        //Copiando para o array original
+        for(int i = 0; i < n; i++) {
+            personagemA[i] = ordenado[i].clone();
+        }
+}
+
+public class Counting {
+	static Countingsort lista = new Countingsort(1000);
+
+	public boolean isFim(String frase) {
+		boolean isFim = true;
+		if (frase.length() == 3) {
+				if (frase.charAt(0) == 'F' && frase.charAt(1) == 'I' && frase.charAt(2) == 'M') {
+				isFim = false;
+				}
+			}
+		return isFim;
+	}
+
+	public void setPersonagem(String strArquivo, Personagem personagem) throws Exception{
+            personagem.acharNome(strArquivo);
+            personagem.altura(strArquivo);
+            personagem.peso(strArquivo);
+            personagem.corDoCabelo(strArquivo);
+            personagem.corDaPele(strArquivo);
+            personagem.corDosOlhos(strArquivo);
+            personagem.nascimento(strArquivo);
+            personagem.genero(strArquivo);
+            personagem.homeworld(strArquivo);
+     }
+
+     public void escreverNoArquivo(String conteudo) throws Exception{
+		try {
+			File diretorio = new File("655985_countingsort.txt");
+			FileWriter arq = new FileWriter("655985_countingsort.txt");
+			arq.write(conteudo);
+			arq.close();
+
+		} catch (Exception e) {
+			MyIO.println("DEU PAU NO ARQUIVO");
+		} 
+
+	}
+
+	public void main(String args[]) throws Exception{
+		long time1 = System.currentTimeMillis();
+
+		String[] entrada = new String[1000];
+		String strArquivo = new String();		
+		int quantidadeEntrada = 0;
+
+
+		do {
+
+			entrada[quantidadeEntrada] = MyIO.readLine();
+
+		} while (isFim(entrada[quantidadeEntrada++]) == true);
+		quantidadeEntrada--;
+
+        Personagem[] arquivo = new Personagem[quantidadeEntrada];
+		Personagem[] personagem = new Personagem[quantidadeEntrada];
+
+
+		for(int i = 0; i < quantidadeEntrada; i++) {
+			arquivo[i] = new Personagem();
+			String convertida = new String(entrada[i].getBytes("ISO-8859-1"), "UTF-8");
+	        strArquivo = arquivo[i].lerArquivo(convertida);
+	        personagem[i] = new Personagem();
+			setPersonagem(strArquivo, personagem[i]);
+			lista.inserirFim(personagem[i]);
+		}
+
+		lista.Counting();
+
+		Personagem print[] = lista.getPersonagemA();
+		for(int i = 0; i < lista.getN(); i++) {
+			print[i].print();
+		}
+		long time2 = System.currentTimeMillis();
+		long tempo = (time2 - time1);
+		String matricula = "655985";
+		String conteudo = matricula + "\t" + tempo + "\t" +lista.getQuantidadeDeMovimentacoes();
+		escreverNoArquivo(conteudo);
+		
+
+	}
+}
